63b83fac997206611c676dfbf1647c478c31e7a5
axenic02 <85927494+axenic02@users.noreply.github.com>
2023-03-16 00:26:24 +0700

    uploaded icon

--- src/Result.js
+++ src/Result.js
@@ -1,11 +1,12 @@
 import './Result.css';
+import logo from './assets/logo.svg';
 
 function Result(){
     return(
         <div className='container'>
             <div className='head'>
                 <div className='header'>
-                    
+                    <img src={logo} alt="logo" width={213} height={36}/>
                 </div>
             </div>
         </div>
