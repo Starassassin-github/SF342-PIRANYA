d08cb0be7ae50d17cd2cee35a5b57d98031dfd2d
axenic02 <85927494+axenic02@users.noreply.github.com>
2023-03-16 00:04:27 +0700

    Create css & page

---
+++ src/Result.js
@@ -0,0 +1,16 @@
+import './Result.css';
+
+function Result(){
+    return(
+        <div className='container'>
+            <div className='head'>
+                <div className='header'>
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default Result;
